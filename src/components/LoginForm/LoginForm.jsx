ea79213ec57bfc5edb36fce52b824d85674c6324
Jani Hellström <45870844+janihell@users.noreply.github.com>
2024-01-13 14:39:42 +0200

    Login logic elevated
    Functional handling moved to LoginPage (prev. LoginForm

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -7,45 +7,10 @@ import {
   Button,
 } from 'react-bootstrap';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
 import './LoginForm.css';
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
 
-const LoginForm = () => {
-  const navigate = useNavigate();
-
-  const initialValues = {
-    email: '',
-    password: '',
-  };
-
-  //Simple validation
-  const validationSchema = Yup.object({
-    email: Yup.string().email('Invalid email address').required('Required'),
-    password: Yup.string()
-      .required('No password provided.')
-      .min(8, 'Password is too short - should be 8 chars minimum.'),
-  });
-
-  //Submit handler – axios example at the bottom
-  const onSubmit = async (values, { setSubmitting, resetForm }) => {
-    //Token acceptance dummy
-    const token = 'jwt_token_dummy';
-
-    //Storing token
-    localStorage.setItem('jwt_token', token);
-
-    //TEST
-    console.log({ user: values, token: token });
-
-    //Navigation Dummy
-    navigate('/groups');
-
-    resetForm();
-    setSubmitting(false);
-  };
-
+const LoginForm = ({ submission, initialValues, validationSchema }) => {
   return (
     <div className='full-height-center'>
       <Container className='fixed-height-container'>
@@ -56,7 +21,7 @@ const LoginForm = () => {
               <Formik
                 initialValues={initialValues}
                 validationSchema={validationSchema}
-                onSubmit={onSubmit}
+                onSubmit={submission}
               >
                 <Form>
                   <BootstrapForm.Group controlId='email'>
@@ -109,37 +74,3 @@ const LoginForm = () => {
   );
 };
 export default LoginForm;
-
-/* DUMMY CALL FOR RREFERNCE *****
-
-
-const onSubmit = async (values, { setSubmitting }) => {
-  try {
-    // Dummy API call
-    const response = await axios.post('https://api.example.com/login', values);
-
-    // Assuming the API returns a token
-    const { token } = response.data;
-
-    // Store the token in localStorage
-    localStorage.setItem('token', token);
-
-    // Your form submission logic goes here
-
-    console.log('API Response:', response.data);
-  } catch (error) {
-    // Handle API call error
-    console.error(
-      'API Error:',
-      error.response ? error.response.data : error.message
-    );
-  } finally {
-    setSubmitting(false);
-  }
-};
-
-
-
-
-
-*/
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,8 +2,48 @@ import React, { useEffect, useState } from 'react';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import { useNavigate } from 'react-router-dom'; // Import useHistory
 
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
+
 const LoginPage = () => {
+  //Inits
   const navigate = useNavigate();
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const initialValues = {
+    password,
+    email,
+  };
+
+  //Simple validation
+  const validationSchema = Yup.object({
+    email: Yup.string().email('Invalid email address').required('Required'),
+    password: Yup.string()
+      .required('No password provided.')
+      .min(8, 'Password is too short - should be 8 chars minimum.'),
+  });
+
+  //Submit handler – axios example at the bottom
+  const onSubmit = async (values, { setSubmitting, resetForm }) => {
+    //Token acceptance dummy
+    const token = 'jwt_token_dummy';
+
+    //Storing token
+    localStorage.setItem('jwt_token', token);
+
+    //TEST
+    console.log({ user: values, token: token });
+    console.log(email, initialValues);
+
+    //Navigation Dummy
+    navigate('/groups');
+
+    resetForm();
+    setSubmitting(false);
+  };
+
+  // const navigate = useNavigate();
 
   // useEffect(() => {
   //   const token = localStorage.getItem("jwtToken");
@@ -13,7 +53,47 @@ const LoginPage = () => {
   //   }
   // });
 
-  return <LoginForm />;
+  return (
+    <LoginForm
+      submission={onSubmit}
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+    />
+  );
 };
 
 export default LoginPage;
+
+/* DUMMY CALL FOR RREFERNCE *****
+
+
+const onSubmit = async (values, { setSubmitting }) => {
+  try {
+    // Dummy API call
+    const response = await axios.post('https://api.example.com/login', values);
+
+    // Assuming the API returns a token
+    const { token } = response.data;
+
+    // Store the token in localStorage
+    localStorage.setItem('token', token);
+
+    // Your form submission logic goes here
+
+    console.log('API Response:', response.data);
+  } catch (error) {
+    // Handle API call error
+    console.error(
+      'API Error:',
+      error.response ? error.response.data : error.message
+    );
+  } finally {
+    setSubmitting(false);
+  }
+};
+
+
+
+
+
+*/
