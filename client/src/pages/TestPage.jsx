635c9ae55a5df01d715a944c23e3d292ff93692e
Jani Hellström <45870844+janihell@users.noreply.github.com>
2024-03-28 13:57:32 +0200

    First iteration of totalAmount track changes
    Kommentteina myös dynaaminen pohja: eli elementti, joka mäpätään memberien tietojen mukaan, pohjat dynaamiseen state-päivitykseen ja fieldien valueiden settaamiseen ja niin edes päin.

--- client/src/pages/TestPage.jsx
+++ client/src/pages/TestPage.jsx
@@ -1,74 +1,134 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router-dom';
-import * as Yup from 'yup';
+import React, { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
 
-import Modal from 'react-bootstrap/Modal';
-import NewExpenseForm from '../components/NewExpenseComponents/NewExpenseForm';
-//TEST
-import AddExpenseForm from '../components/AddExpenseForm/AddExpenseForm';
+import '../components/LoginForm/LoginForm.css';
 
-const TestPage = () => {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  const navigate = useNavigate();
+const MyForm = () => {
+  // 3 Local states to keep track of the numbers
+  const [totalAmount, setTotalAmount] = useState(0); //user set
+  const [remainingAmount, setRemainingAmount] = useState(0); //calculated
+  const [participantAmount, setParticipantAmount] = useState(0); //user set. Tästä dynaaminen array
 
-  const handleSubmit = (values, { resetForm }) => {
-    try {
-      // Handle form submission here
-      console.log('Form values:', values);
-      toast.success('Expense added ✅ ');
-      resetForm();
-      handleClose();
-      navigate('/groups'); // add navigation to the same group when merging
-    } catch (err) {
-      console.log(err);
-    }
+  //********* TOTAL AMOUNT *******//
+  const handleTotalAmountChange = (e, setFieldValue) => {
+    const newValue = parseFloat(e.target.value);
+
+    //Handling total amount
+    setTotalAmount(newValue);
+    setRemainingAmount(newValue);
+    setFieldValue('totalAmount', newValue);
+
+    //Resetting participant value when total changes for UX reasons
+    setParticipantAmount(0);
+    setFieldValue('participantAmount', 0);
   };
 
-  const initialValues = {
-    //firstName: '',
-    //lastName: '',
-    //email: '',
-    creator: '',
-    group: '', //can we add it here?
-    description: '',
-    amount: '',
-    payer: '',
-    divisionType: 'split equally',
-    shares: '',
+  //********* USER AMOUNTS *******//
+  const handleParticipantChange = (e, setFieldValue) => {
+    // Declaring *all* required variables
+    const participantAmount = parseFloat(e.target.value);
+    const newRemainingAmount = totalAmount - participantAmount;
+
+    // Updating states and userfields
+    setRemainingAmount(newRemainingAmount);
+    setParticipantAmount(participantAmount);
+    setFieldValue('amountLeft', newRemainingAmount);
+    setFieldValue('participantAmount', participantAmount);
+
+    //LATER: DYNAMIC UPDATING BASED ON THE FIELD ID (i.e. right field & remaining amounts are updated)
+
+    const elementID = e.target.id; //Getting the id of the field that was changed
+    const newValue = e.target.value; //Getting the changed value for state and fieldset
+
+    // --> setFieldValue(elementID, newValue)
+    // --> setParticipantAmount (iteroituvasti)
   };
 
-  const validationSchema = Yup.object({
-    // firstName: Yup.string().min(1, 'Invalid name').required('Required'),
-    // email: Yup.string().email('Invalid email address').required('Required'),
-    // password: Yup.string()
-    //   .required('No password provided.')
-    //   .min(8, 'Password is too short - should be 8 chars minimum.'),
-  });
+  //Dummy members for dynamic rendering and naming – LATER
+  const members = [
+    {
+      name: 'kissa',
+      share: '',
+    },
+    {
+      name: 'kassa',
+      share: '',
+    },
+  ];
+
+  // **** DYNAMIC RENDERING TEMPLATE FOR LATER –– WORKS *****
+
+  // const dynamicUserInputFields = members.map((member, index) => (
+  //   <div>
+  //     <label htmlFor='participantAmount'>Participant Amount:</label>
+  //     <Field
+  //       type='number'
+  //       id={`participantAmount+${member.name}`}
+  //       name='participantAmount'
+  //       value={participantAmount === 0 ? '' : participantAmount}
+  //       placeholder='0'
+  //       onChange={(e) => handleParticipantChange(e, setFieldValue)}
+  //     />
+  //   </div>
+  // ));
 
   return (
-    <>
-      <Button variant='primary' onClick={handleShow}>
-        Add new expense
-      </Button>
+    <Formik
+      initialValues={{ totalAmount: 0, amountLeft: 0, participantAmount: 0 }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ values, setFieldValue }) => (
+        <Form>
+          <div>
+            <label htmlFor='totalAmount'>Total Amount:</label>
+            <Field
+              min='0'
+              type='number'
+              id='totalAmount'
+              name='totalAmount'
+              value={totalAmount === 0 ? '' : totalAmount}
+              placeholder='0'
+              onChange={(e) => handleTotalAmountChange(e, setFieldValue)}
+            />
+          </div>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Add new expense</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <NewExpenseForm
-            initialValues={initialValues}
-            handleSubmit={handleSubmit}
-            validationSchema={validationSchema}
-          />
-        </Modal.Body>
-      </Modal>
-    </>
+          {/* Other fields for participants */}
+
+          {/* DYNAMIC TEMPLATE FOR LATER  | MAPPING DONE ABOVE */}
+          {/* <div>{dynamicUserInputFields}</div> */}
+
+          <div>
+            <label htmlFor='participantAmount'>Participant Amount:</label>
+            <Field
+              type='number'
+              id={`participantAmount`}
+              name='participantAmount'
+              value={participantAmount === 0 ? '' : participantAmount}
+              placeholder='0'
+              onChange={(e) => handleParticipantChange(e, setFieldValue)}
+            />
+          </div>
+
+          <div>
+            <label htmlFor='amountLeft'>Amount Left:</label>
+            <Field
+              type='number'
+              id='amountLeft'
+              name='amountLeft'
+              value={remainingAmount}
+              disabled
+            />
+          </div>
+
+          <button type='submit' disabled={remainingAmount !== 0 ? true : false}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
-export default TestPage;
+export default MyForm;
